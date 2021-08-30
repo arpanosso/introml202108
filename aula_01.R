74b55bafe73feb8146504f7920339d9c4074b0d9
arpanosso <arpanosso@yahoo.com.br>
2021-08-30 19:50:13 -0300

    Initial commit

---
+++ aula_01.R
@@ -0,0 +1,52 @@
+# INTAlAÇÃO DE PACOTES
+# install.packages('tidyverse')
+# install.packages('tidymodels')
+# install.packages('rmarkdown')
+# install.packages('knitr')
+# install.packages('ISLR')
+# install.packages('glmnet')
+# install.packages('xgboost')
+# install.packages('randomForest')
+# install.packages('ranger')
+# install.packages('rpart')
+# install.packages('rpart.plot')
+# install.packages('pROC')
+# install.packages('vip')
+# install.packages('modeldata')
+# install.packages('usemodels')
+# install.packages('tidypredict')
+# install.packages('jpeg')
+# install.packages('MASS')
+# install.packages('DataExplorer')
+# install.packages('skimr')
+# install.packages('naniar')
+# install.packages('patchwork')
+
+# MACHINE LEARNING, máquina aprendendo, é o nome da área de pesquisa, para falar
+# disso suamos machine learning, cunhado por Ahtur Samuel em 1959, mas usamos
+# sinonimo de modelagem preditiva: È UM FRAMEWORK de análise de dados que visa
+# gerar a estimativa mais precisa possivel apra uma quantidade ou fenômeno.
+# é um conjunto de téncia euristicas e ferramentas que tem o objeitovo de grar
+# uma estimativa precisa, ou mais precisa possivel para algo que vc está querendo
+# prever. Não é um método só, é um conuunto de ferramentas com vária abordagem
+# para um único problema. para fazer um tipo de análise de dados.
+
+# Aplicações
+# quem vai cancelar linha churn
+# quem vai parar de me pagar - ter uma previsão
+# previsãod e demanda, quanto de produto vamos vender para planejar a produção
+# prever os preços,
+# preveo o tempo, meteorológicas
+# identificar imagem de raioX para ver se tem doença ou não
+# o carro autônomo vai aprender a identificar as coisas, para ser usado
+# Prever a série temporal, como desenprego
+# teste A/B, duas formar e ver qual funciona melhor para um produto, ou um site
+# eficacia de vacinas
+# impacto de políticas públicas
+# várias aplicações
+# Bolsa de valores não se usa ML pois é meio aleatório e não consegu prever
+# a resposta é baixa, usa para trade de alta frequência, mas no A~mbito geral
+# temos fenômenos totalmetne aleatório.
+
+
+
