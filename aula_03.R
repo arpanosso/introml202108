45c385be9b7e829b90927ff513d5f0fdf8f55637
Alan R Panosso <arpanosso@yahoo.com.br>
2021-09-16 16:53:32 -0300

    aulas 03 e 04

---
+++ aula_03.R
@@ -0,0 +1,19 @@
+# Fluxo de Trabalho
+# 1 olha o hitograma da variavel,
+# no caso dos exemplo do preço (segunda metade da aula), fazer o LOG10
+# amexia o problema da cauda
+# tem ue testar a transformação no mundo real
+# pode testar a raiz tmb
+
+# 2 Não podemos usar alguams variáveis exemplo, tipo de venda, ano de venda
+# mes de venda, se vc quer prever vc não sabe quando vendeu, essa variáveil
+# não estará no banco de dados. São dados pós factos
+
+# 3 DISCRETIZAÇÃO
+#  agrupar niveis de categorais com poucos valores, não usamos todas as
+# categorias.
+
+# Olhar a interação entre as categorais e a var resposta o gráfico e o ajuste para
+# cada categorias
+# no caso do X e do Y fazer a interação tmb
+
