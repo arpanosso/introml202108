664f8cf787c94af9c8d16627e52d251046fb52c5
arpanosso <arpanosso@yahoo.com.br>
2021-09-16 18:42:55 -0300

    aulas ate 03

--- aula_01.R
+++ aula_01.R
@@ -48,5 +48,51 @@
 # a resposta é baixa, usa para trade de alta frequência, mas no A~mbito geral
 # temos fenômenos totalmetne aleatório.
 
+# O ML tem um conjuunto muito grande de técnicas
+# 1- Aprendizado Não Supervisionado - foca em estudar problemas que não temos
+# algo preciso na previsão, exemplo compressão de imagem,
+# 2- Aprenizado supervisionado - quando vc tem uma variável respostas
+# 3- Parendizado pro reforço, vc tem a resposta mas existem uma interação com
+# o ambiente, como exemplo Jogar Go, Xadrez, ele tem que interagir com o jogo
+# e saber o resultado de cada ação que ele tem e não sõ o jogo inteiro. O algoritmo
+# interagem com o ambiente,
+
+# NOSSO FOCO SERA APRENDIZADO SUPERVIZIONADO
+# como consultores, como podemos aumentar a venda por markegind
+# temos invstimento em jornal rádio e TV e o quanto vendeu com o investimento
+# QUantas vendas vamos ter se eu investir X e em qual mídia é melhor eu alocar o
+# meu orçamento.
+# O modelo dá uam regra para acertar o máxim possível
+
+# outro exemplo, quais as pessoas que vão atrasar as parcelasww
+# temos o valor da parcela e a variavel resposta é zero se não atrasou e
+# 1 se ela atrasou, temos um problmea de classificação, e temos outras caracterís
+# ticas do problema, QUAL A POBABILIDADE DE ATRASRA A FATURA NO MES QUE VEM
+
+# Então temos dois tipos de problemas
+# REGRESSÂO (é contínua) E CLASSIFICAÇÂO (é categórica)
+# regressão linear é uma função que usamos, pois ela dá retas/planos
+
+# árvores de decição, a cara é diferente, ela vai cortando pedaços e depoif fica constante
+# faz os degrauzinhos
+
+# Na regressão log´sitica, temos as curvas , uma reta no espaço dos loistos
+# se for árvore fica degrauzinhos
+
+# Na classificação temos a probabilidade de ser algo
+# e erro tem o logo associado no calculo
+# quase tudo é igual, mas mudamos uns detalhes
+
+# o objetivo do curso será sempre a predição, abriremos mão da interpretação
+# Ordenação - Média - variãncia - Explicação
+# vamos focar em ordenação e média
+
+# Métricas - Qual a melhor
+# Raiz do erro quadrático médio RMSE
+# raiz (1/n Soma (y - yb)² )
+
+
+
+
 
 
---
+++ aula_02.R
@@ -0,0 +1,5 @@
+# Overfitting - sobreajuste
+# series temporais pegamos o final da base para o teste
+# não seleciona pojntos aleatórios
+# com localização deve-se fazer isso tmb
+
