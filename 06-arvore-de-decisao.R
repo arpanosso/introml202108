b5c30a4dc13a3b1f22b18dac04409c08ffe03032
arpanosso <arpanosso@yahoo.com.br>
2021-09-16 22:04:51 -0300

    aula

--- 06-arvore-de-decisao.R
+++ 06-arvore-de-decisao.R
@@ -1,3 +1,85 @@
+# 1 - Arvores de decisão é o princuipal tópico
+# se fazemos uma arvore longa, podemos fazer varias aleatorias so que
+# são varias arvorezinhas
+# gradient boosting é passo a passo ajustado e separado, mas a ideia
+# já oode ser usada , sem medo de ficar a deriva do algoritmico
+# são varias arvores linkadas
+# não usar ID e nome para a previsão
+# ávores tem duas partes
+# 1 - PERGUNTA (NÓ) - de sim ou não e vai separando o BD em duas partes
+# 2 - RESPOSTAS (FOLHAS) - que toma uma decisão com uma probabilidade.
+# Primeiro nó é o Nó RAIZ - sobre a base inteira
+# Pergunta par as explicativas e reponde com as respostas.
+# a média é o degrau da curva, entendido
+# ela da uma coluna igual da logistica
+# NOME DO PACOTE rpart mas foi pensado a partir do algo recursivo.
+# ou seja recursive partittioning, partionamento recursivos do seu BD
+# Arvores Binárias - Binárias pois as perguntas são sim e não, particiona em
+# 2 vai buscando binariamente. não por conta de zeros e uns
+# Poderiamos ter arvores ternarias, mas os resultados não s]ao tao bons
+# com os binarios.
+
+# Mas qual a melhor pergunta? o algoritmos lista todas as possiveis perguntas
+# a se fazer ao banco de dados, mas de uma maneira inteligentes,
+# ele não varre tudos, ele ordena e faz pergunta no meio do caminho para
+# uma variável x. A combnação dentro das pergunas é por variavesl
+# sem combinçao dentro delas, e depois passa para outra
+# e ele mede o ganho de informação, o qnt foi boa aquela pergunta, o quenta
+# você aprende com aquela pergunta, o GINI e a ENTROPIA são as medidas
+# para ver a melhor pergunta para uma variavel
+
+# Impureza e Ganho de Informação, GINI Entropia e Variancia são conceitos
+# ligados à função de CUSTO, que deve ser minimizada. NA árvores, qual o
+# conjunto de perguntas que reduz a funçãod e custo, que aumenta o ganho,
+# que reduz a impureza da árvore.
+# GANHO DE INFOMACO - comparação entre o nó principal e os nos da esquerda
+# e da direita, é o quanto erramos antes e o quanto erramos depois de passar
+# por o nó.
+# agora precisamos desenvolver a noção de impureza - que tem a ver com a
+# variável resposta se a folha tem uma categoria, ela é PURA, se tiver mais de
+# uma categoria ela tera impureza maior que sero. Na folha PURA todo mundo recebeu
+# ou vai receber a mesma classificação, acertamos 100%, ou seja omeu Erro,
+# meu custo é ZERO, arvores com folhas puras acerta 100%
+# se tenho 6 pessoas, posso ter 6 arvores, estou decorando a base de dados
+# overfiting de mais fino trato, queremos folhas puras, mas não queremos
+# muito puro, o mesmo com o polinômio, não podemos acertar tudo. NO
+# caso de pouca inofrmação, ou se investe mais dinheiro, ou a conculsão será
+# conclusivo se as evidencias forem muito fortes. AS vezes pode-se fazer
+# avaliação ponto a ponto e ver o que aocnteceu. As cees a bela descritiva e o
+# modelo de regressão xplica suficientemnte o seus dados.
+# VOltando para a Impureza: usamos para calcular por folha, a formula
+# de cada uma das debaisxo é feito por folha, sempre
+# GINI - Classificação (1 - Sum(p1^2))
+# ENTROPIA - CLassificação -SUM(pilog(pi))
+# VARIANCIA - Regressão 1/n SUM((yk - hyk)^2)
+# Esse indices são utilizados para escolher as melhreos perguntas
+# o tresholds dentro de uma variavel, e escolhe a pergunta com maior ganho de
+# informação, geralmente escolhemos uma e pronto,
+# a gente para de fazer pergunta que elas fazem o modelo errar
+# mais, deixa ela sofisticada e tentamos na base de teste, as arvore fica cabulosa
+# mas não funfa na base de teste.
+# Hiperparametros
+# min_n = ´numero minido de observaços minimos que tem que ter no nó
+# tem que ter gente suficiente para fazer a pergunta - é determinado do Cross-valitations
+# min_n=0 complexa, min_n alto é pouco complexa.
+
+# tree_depth é a profundidade da árvore, cada pergunta é uma profundidade
+# as vezes mais que 5 perguntas é ruim, quanto mais profundidade mais complsxa
+# será a sua árvore.
+
+# cost complexit, custo da complexidade, penaliza para fazer uma pergunta
+# o ganho deve ser maior que um valor para uma pergunta ser adicionado no modelo
+# as vezes não vale a pena fazer a pergunta para ter um ganho que pode ate
+# ser fruto do acaso. em analogia seria o lambda do lasso com a diferença
+# de o móduoo de T ser a qndta de folhas no modelo...
+
+# 2 - relação viés variancia
+
+# 3 - random Forest
+
+# 4 - Gradiente Boost
+
+
 library(tidymodels)
 library(tidyverse)
 library(rpart)
