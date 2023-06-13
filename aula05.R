1ccdd5663cba214660dd9b5d7541098268332fc1
Alan Rodrigo Panosso <arpanosso@yahoo.com.br>
2023-06-13 05:50:43 -0300

    aula 5

---
+++ aula05.R
@@ -0,0 +1,97 @@
+'# super complexa
+# não são tão comuns
+# grgressao tem 300 anos
+# arvores tem poucos anos nasceu na computação
+# tem alguns defeitos
+# as evoluções reoslvem os modelos
+# RF, GBT, Xgboost
+# na vdd é um questionário que ocorre na argore
+# RF - 500
+# GB - Tem formula de como mescla
+
+# na pratica vamos trocar o modelo
+# 2 livros de statistical learning
+
+# arvore consiste de ao ínves de traçar uma reta
+# voufazer uma escada
+# Y é aproximado por F(X)
+# corresposndencia com conjunto de pergunta
+# questionario de pergunta encadeada
+# Akinator é uma árvore de decisão
+# o ecadeamento do Akinator é um questionario que é uma
+# árvore -classificação e de greressão
+#
+# Pergunta é o Nó
+# Resposta é o Folha - final da árvore
+# encontrar as perguntas que separem melhor a distribuição das
+# resposta, as vezs é só 0 e 1.
+# o número de pergunta é a aaltura da árvore
+# se fizer 3, vai até a altira 3
+# PREVISÂO DE DIABETES
+# exemplo, vamos extrair a melhor possivel
+# cada vez que chego na folha achance de errar é menor possivel
+# varias primeiras perguntas
+# priemira faz a mais importates que sepaaram melhor
+# o grupo
+# GLICOSE deve ser a mais importate
+# alta tem diabetes, baixa configura outras
+# segunda poderia ser hipertenção...mas ele compara todas
+# ele ve que a melhor é sobre glicose...tmb...
+# não se pode demorar muito para fazer perguntas boas
+# melhor o meu modelo.
+
+# ná pratica, quais perguntas e qeum ordem sao paramenters para
+# serrem ajustados. poq não inverteu as perquntas?
+# as segregações são feitas para segregar o melhor possivel.
+# escolhe a pergunta em funçaõ dos dados
+
+#HIPERPARÂMETRO - quantas perguntas posso fazer
+
+#Quais as perguntas boas?
+# escolhe a melhor que segregue em dois grupos e assim por diante
+# dentro de cada grupo ele tenta segregar
+# cada pergunta é uma variável
+# mede-se a qualidade de diferença
+# se der ero gini e entropia da zero
+# escolhe as perguntas de deixa grupos mais diferentes
+
+# não se pode ter poucas folhas numa folha
+# se não ele faz folhas com uma so obervação
+
+#profundidade, numero de perguntas
+#quao mais profundo mais a change de chegar numa situação que
+# cso aocntece na base de dados ai eu vou oferfita
+#
+# custo de complexydade, pra fazer nova, tem que ter uma diferneça mini
+# entre os grupos que estão sem fitados
+# qq ruido não pode criar uma pergunta nova
+# temos que ter um corte mínimo, que são suficientemente
+# diferentes é uma nota de corte
+
+# variave numeria cuidade com altura
+# modelo de classifica]
+# tem que ter no bolso
+# 1-good/bad, acuraci não pode ser menor que isso
+# strata é importate e vc colocar a variavel resposta
+
+#stepnovel é imporaten para alvpres,
+# naõ precosa de polinomi, nem dummy
+# altura grande pega a aprte polinomeisa
+# missim é tratado com categoraia no rpart
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
