9899f707ff8ddce2c6ed1cbbc7c1a440fb8638c7
Alan Rodrigo Panosso <arpanosso@yahoo.com.br>
2023-08-22 09:54:51 -0300

    aula 2 - parsnip

--- aula05.R
+++ aula05.R
@@ -155,9 +155,181 @@ temos que ter uma boa e prnto, escolhas mais importantes pra fazer posteriorment
 
 
 
+# AULA 2
+ML - programa de computado que constroi algo que faz algo pra gente
+cp devonlve o que ele acha que é uma boa relação entre x e y
+ml é programa q se auto progrmaa
+
+supervisiona - tem y e x qual afunção boa para pegar x e leve no y com erro
+pequeno
+
+f é modelo de ML (mas é programa que encontra f)
+tem variso formatos de f, precisamos de boas aproximações
+
+tem varios jeitso de achar f o q diferes osprograma é que casda procura uma certa
+forma, cara de função. Teoricamente, nem sempre a linear é a melhor....os de arvore a filo
+sofia é outra, é diferente, ao invez de reta faz escadinha. mas a logiaca e a mesma
+
+relacçao DESEMPENHO E INTERPRABILIDADE 0 VALE A PENA
+temos varias jeitos de manipular os programas que acham a F , tydimofdels
+e sckitleanr adotam o mesmo formado.
+podemos trocar as f, essa funcionalidade é muito bom pois queremos comparar os modelos tods
+todos os programas acabam sendo parecidos pois a comunidade convergiu para um
+formato parecido e é o que se vai mostrar no r
+
+
+tidymodels - metapacote, conjunto de vários outros pacotes
+tem vários pacotexinhos que traz uma caixa de ferramenta de varias funcionalide
+utieis para achar fs, e varios cuidados que devemos tomar e esse pacote
+tem as rotidas para ficar mais obejtitos e não perder muito
+tempo programando.
+
+exemplo abaixo é o modelo de preço justo - modelo de apressamento do
+aluguel de apartamento - quitandar - tem que fazer o preço de aluguel
+e se faz o prcço de mercado o exemplo abaixo será de exemplo de diamante.
+qual a tendencia de dados.
+'
+
+# Pacotes ------------------------------------------------------------------
+
+library(ggplot2)
+library(skimr)
+library(tidymodels)
+
+# Dados -------------------------------------------------------------------
+data("diamonds")
+
+# ---------------------------------------------------------------------
+glimpse(diamonds)
+skim(diamonds) # capivara dos dados
+
+qplot(x, log(price), data = diamonds)
+qplot(price, data = diamonds, geom = "histogram")
+qplot(x, data = diamonds, geom = "histogram")
+
+# Precisamos passar pro R:
+# 1. A f que queremos usar
+# 2. Ajustar a f para um conjunto de dados
+
+# ----------------------------------------------------------------------
+# Passo 1: Especificações de
+# a) a f (a hipótese) com seus respectivos hiperparâmetros;
+# b) o pacote 'motor' (engine);
+# c) a tarefa/modo ("regression" ou "classification").
+
+# Porque o tidymodels é legal?
+
+# no dplyr faríamos:
+
+# dados %>%
+# mutate(nova_coluna = coluna_anterior+1) %>%
+# group_by(grupo) %>%
+# summarise(media_coluna = mean(nova_coluna))
+
+# Essas funções sao do parsnip.
+# Sempre 3 passos
+?linear_reg
+especificacao_modelo <- linear_reg() %>% # forma funcional - desenho da f
+  set_engine("lm") %>% # qual pacote usar.
+  set_mode("regression") # qual tipo de modelo
+# essas definições são redundantes, mas servem pra deixar
+# claro o que estamos fazendo.
+
+especificacao_modelo
+
+# Outros exemplos...
+?decision_tree
+especificacao_modelo_arvores <- decision_tree() %>%
+    set_engine("spark") %>% # ou rpart
+    set_mode("regression")
+#
+# especificacao_modelo <- rand_forest() %>%
+#   set_engine("ranger") %>%
+#   set_mode("regression")
+
+'https://www.tidymodels.org/find/parsnip/'
+# --------------------------------------------------------------------
+# Passo 2: Ajuste do modelo
+
+diamonds <- diamonds %>% mutate(
+  log_price = log(price),
+  flag_maior_que_8 = ifelse(x > 8, "sim", "nao")
+)
+
+modelo <- especificacao_modelo %>%
+  fit(log_price ~ x, data = diamonds)
+# essa é a f
+
+print(modelo)
+
+# esses dois são iguais
+reg1 <- extract_fit_engine(modelo)
+reg2 <- lm(price ~ x, data = diamonds)
+
+# --------------------------------------------------------------------
+# Passo 3: Analisar as previsões
+
+novo_x <- data.frame(x = 7)
+predict(modelo, new_data = novo_x)
+# ^ esse é o "programa que se programou sozinho"
+
+so_o_x <- diamonds %>% select(x, flag_maior_que_8)
+pred <- predict(modelo, new_data =  so_o_x)
+
+
+# aqui só vamos usar tidyverse p/ baixo
+
+diamonds_com_previsao <- diamonds %>%
+  add_column(pred) %>%
+  mutate(.pred_price = exp(.pred))
+
+# Pontos observados + curva da f
+diamonds_com_previsao %>%
+  filter(x > 0) %>%
+  sample_n(1000) %>%
+  ggplot() +
+  geom_point(aes(x, price), alpha = 0.3) +
+  geom_point(aes(x, .pred_price), color = "red") +
+  theme_bw()
+
+# Observado vs Esperado
+diamonds_com_previsao %>%
+  filter(x > 0) %>%
+  ggplot() +
+  geom_point(aes(.pred_price, price)) +
+  geom_abline(slope = 1, intercept = 0, colour = "purple", size = 1) +
+  theme_bw()
+
+diamonds_com_previsao %>%
+  filter(x > 0) %>%
+  sample_n(1000) %>%
+  ggplot() +
+  geom_point(aes(price, price - .pred_price))
+
+# Como quantificar a qualidade de um modelo?
+
+library(yardstick)
+
+metrics <- metric_set(rmse, mae, rsq)
+
+# Métricas de erro
+diamonds_com_previsao %>%
+  metrics(truth = price, estimate = .pred_price)
+
+
+diamonds_com_previsao %>%
+  mae(truth = price, estimate = .pred_price)
+diamonds_com_previsao %>%
+  rsq(truth = price, estimate = .pred_price)
+diamonds_com_previsao %>%
+  rmse(truth = price, estimate = .pred_price)
 
 
 
+
+
+'
+#Aula 4
 
 super complexa
 # não são tão comuns
